06cdc152d54e7b746bef1709d3df88e245b1c192
oregs <36188245+oregs@users.noreply.github.com>
2019-04-15 17:38:30 +0100

    Update connect.php

--- admin/components/connect.php
+++ admin/components/connect.php
@@ -1,6 +1,6 @@
 <?php
 // connect to the database
-$db = mysqli_connect('localhost', 'root', 'password', 'asg');
+$db = mysqli_connect('localhost', 'bbdewkcaed', '25MymGTQ6Z', ' bbdewkcaed');
 
 if ($db->connect_error) 
 	die($db->connect_error);
