92f5d28d45861185d84be72382666cd4a1a05cf7
oregs <36188245+oregs@users.noreply.github.com>
2019-04-15 17:37:58 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '25MymGTQ6Z', ' bbdewkcaed');
+$db = mysqli_connect('localhost', 'bbdewkcaed', '25MymGTQ6Z', ' bbdewkcaed');
 
 if ($db->connect_error) 
 	die($db->connect_error);
