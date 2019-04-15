aec29c0e94acd676d81748563b6fbd0935491495
oregs <36188245+oregs@users.noreply.github.com>
2019-04-15 17:36:29 +0100

    Update connect.php

--- tutor/notification/connect.php
+++ tutor/notification/connect.php
@@ -1,4 +1,9 @@
-<?php 
-//connect.php;
-$conn = mysqli_connect("localhost", "root", "password", "asg");
+<?php
+// connect to the database
+$db = mysqli_connect('localhost', 'bbdewkcaed', '25MymGTQ6Z', 'bbdewkcaed');
+
+if ($db->connect_error) 
+	die($db->connect_error);
+
+
 ?>
